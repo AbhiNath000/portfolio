a35e047f6398a671106de7314f51c22afe085ece
Abhishek Nath <nathabhishek80@gmail.com>
2024-07-28 23:26:56 +0530

    Initial commit of portfolio website

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    // Smooth scrolling for navigation links
+    $('nav a').on('click', function(event) {
+        if (this.hash !== '') {
+            event.preventDefault();
+            const hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    // Reveal sections on scroll
+    function reveal() {
+        var reveals = document.querySelectorAll('.reveal');
+        for (var i = 0; i < reveals.length; i++) {
+            var windowHeight = window.innerHeight;
+            var elementTop = reveals[i].getBoundingClientRect().top;
+            var elementVisible = 150;
+            if (elementTop < windowHeight - elementVisible) {
+                reveals[i].classList.add('visible');
+            } else {
+                reveals[i].classList.remove('visible');
+            }
+        }
+    }
+    window.addEventListener('scroll', reveal);
+
+    // Initial reveal
+    reveal();
+});
